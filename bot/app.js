2609e1f8dee39272b5f91f716feebc85929541d3
Артём <legenda333aa@gmail.com>
2024-10-31 14:00:55 +0300

    Изменения

--- bot/app.js
+++ bot/app.js
@@ -1,7 +1,7 @@
 import { Telegraf, Markup } from 'telegraf'
 
 const token = '7319758246:AAGam-VqfuaKwM2ys_CqNe0gjqYyGdviTlc'
-const webAppUrl = 'https://mining-empire-game.web.app'
+const webAppUrl = 'https://mining-empire-game.web.app/'
 
 const bot = new Telegraf(token)
 
@@ -9,8 +9,10 @@ bot.command('start', (ctx) => {
     ctx.reply(
         'Добро пожаловать!',
         Markup.inlineKeyboard([
-            Markup.button.webApp('Open mini-app', `${webAppUrl}?ref=${ctx.payload}`),
-    ]))
+            Markup.button.webApp('Open mini-app', `${webAppUrl}?ref=${ctx.payload}`
+            ),
+        ])
+    )
 })
 
 bot.launch()
