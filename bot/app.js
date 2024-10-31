a40a2987ab363d3654e533abadf146e3275024c2
Артём <legenda333aa@gmail.com>
2024-10-31 11:35:34 +0300

    Add tg bot with docker

---
+++ bot/app.js
@@ -0,0 +1,16 @@
+import { Telegraf, Markup } from 'telegraf'
+
+const token = '7319758246:AAGam-VqfuaKwM2ys_CqNe0gjqYyGdviTlc'
+const webAppUrl = 'https://mining-empire-game.web.app'
+
+const bot = new Telegraf(token)
+
+bot.command('start', (ctx) => {
+    ctx.reply(
+        'Добро пожаловать!',
+        Markup.inlineKeyboard([
+            Markup.button.webApp('Open mini-app', `${webAppUrl}?ref=${ctx.payload}`),
+    ]))
+})
+
+bot.launch()
